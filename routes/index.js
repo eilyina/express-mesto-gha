c2bdd80a88c217279f976fc7e17c339697f31d84
eilyina <novokaterinka@gmail.com>
2023-06-07 01:41:45 +0500

    add joi1

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,15 @@ const router = require('express').Router();
 const { NOT_FOUND } = require('../utils/error-response-code');
 const { createUser, login } = require('../controllers/users');
 const auth = require('../middlewares/auth')
-const { errors } = require('celebrate');
+const { errors, celebrate, Joi } = require('celebrate');
 
 
-router.post('/signup', createUser);
+router.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }),
+}), createUser);
 router.post('/signin', login);
 
 router.use(auth);
