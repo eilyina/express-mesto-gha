73e8315bdd062d8caeda1fed73d69e67b376558d
eilyina <novokaterinka@gmail.com>
2023-06-07 01:32:42 +0500

    add joi

--- routes/cards.js
+++ routes/cards.js
@@ -1,12 +1,25 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+
 const {
   getCards, createCard, getCardById, deleteCard, likeCard, dislikeCard,
 } = require('../controllers/cards');
 
 router.get('/', getCards);
-router.post('/', createCard);
+router.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required(),
+  }),
+}),createCard);
 router.get('/:id', getCardById);
-router.delete('/:id', deleteCard);
+router.delete('/:id', celebrate({
+  // валидируем параметры
+  params: Joi.object().keys({
+    id: Joi.string().hex().length(24),
+  }),
+
+  }),deleteCard);
 router.put('/:id/likes', likeCard);
 router.delete('/:id/likes', dislikeCard);
 
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const { NOT_FOUND } = require('../utils/error-response-code');
 const { createUser, login } = require('../controllers/users');
 const auth = require('../middlewares/auth')
+const { errors } = require('celebrate');
 
 
 router.post('/signup', createUser);
@@ -9,14 +10,13 @@ router.post('/signin', login);
 
 router.use(auth);
 
-
-
 // роуты, которым авторизация нужна
 router.use('/cards', require('./cards'));
-
 router.use('/users', require('./users'));
 
 //router.use('/cards', require('./cards'));
+
+router.use(errors());
 router.use(require('../middlewares/errorHandler'));
 
 router.use('/', (req, res) => res.status(NOT_FOUND).send({ message: 'Неверный url' }));
